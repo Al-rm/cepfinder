f5129f63bc055438d293d418f9efd80b48f3b918
AL <alrm.dev@gmail.com>
2024-08-21 21:45:38 -0300

    CEPapi & minor ajustments

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
+import { useState } from 'react';
 import './styles.css';
+import apicep from './services/apicep';
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
+
+  async function handleSearch(){
+    //INPUT CHECK
+    if (input === '') {
+      alert("Campo de pesquisa vazio.")
+      return;
+    }
+    try {
+      const response = await apicep.get(`${input}/json/`);
+      setCep(response.data);
+      setInput("")
+    }catch{
+      alert("Endereço não encontrado.")
+      setInput("");
+    }
+  }
+
+
   return (
     <>
       <div className="container">
         <div className='title_div'>
           <h1 className='title'>CEPFINDER</h1>
           <div className='container_input'>
-            <input className='input_search' type='text' placeholder="CEP NUMBER..." />
-            <button className='button_search'>
-              🔎
-            </button>
+            <input 
+            type='text' 
+            placeholder="CEP NUMBER..." 
+            value={input}
+            onChange={(e) => setInput(e.target.value) }
+            />
+            <button className='button_search' onClick={handleSearch}>🔎</button>
           </div>
         </div>
 
-        <main className="main">
-          <h2>CEP 000.000-000</h2>
-          <span>Rua</span>
-          <span>Bairro</span>
-          <span>Cidade</span>
-          <span>Estado</span>
-
-
-        </main>
-        
+        {Object.keys(cep).length > 0 && (
+          <main className="main">
+            <h2>CEP: {cep.cep}</h2>
+            <span>{cep.logradouro}</span>
+            <span>{cep.complemento}</span>
+            <span>{cep.bairro}</span>
+            <span>{cep.localidade} - {cep.uf}</span>
+          </main>
+        )}
       </div>
-      
+
     </>
   );
 }
